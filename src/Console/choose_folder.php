5068b0bc0331258a95be9512d5fa4da87f029621
Ahmed-Osama2022 <ahmadosama.2011@gmail.com>
2026-05-16 01:21:19 +0300

    remove test code.

--- src/Console/choose_folder.php
+++ src/Console/choose_folder.php
@@ -6,7 +6,7 @@ function choose_folder()
 {
   // 1- Get the working directory
   $directories = scan_directory();
-  inspect($directories);
+  // inspect($directories);
 
   if (empty($directories)) {
     echo "❌ No folders found in the current directory. ❌\n";
